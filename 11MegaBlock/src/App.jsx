ba9747511db7d9fa4ac5b89d92aabc7d1a24b232
Hemant Bamboriya <dhakadhemant370@gmail.com>
2025-06-27 20:21:38 +0530

    environment setup

---
+++ 11MegaBlock/src/App.jsx
@@ -0,0 +1,17 @@
+import conf from './conf/conf';
+import './App.css'
+
+function App() {
+ 
+console.log(conf.appwriteUrl);
+
+  return (
+    <>
+     <h1>
+      A blog app with appwrite as a Baas
+     </h1>
+    </>
+  )
+}
+
+export default App
