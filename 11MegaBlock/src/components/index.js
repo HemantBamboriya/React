120b6258a987b2942779a30036f622ec00c40c6d
Hemant Bamboriya <dhakadhemant370@gmail.com>
2025-07-02 23:02:42 +0530

    writing component

--- 11MegaBlock/src/components/index.js
+++ 11MegaBlock/src/components/index.js
@@ -1,4 +1,7 @@
-import Header from "./header/header";
-import Footer from "./footer/footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
 
-export {Header,Footer};
+export {Header,Footer,Container,Logo,LogoutBtn};
